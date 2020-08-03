6bb169afa793bfb029546a8335321adfbc269211
Uzoma Ariguzo <u.ariguzo.ua@gmail.com>
2020-08-03 17:57:22 -0400

    Added Prime Numbers

---
+++ Prime Numbers/index.js
@@ -0,0 +1,10 @@
+function isPrime(num) {
+    if (num <= 1) return false; 
+        //5 is larger than 1, so we continue with the function if (num === 2) return true;
+        //5 is not 2, so we continue with the function for (let i= 2; i < num; i++) {
+        if (num % i === 0) {
+        return false;
+        }  
+    return true;
+}
+
